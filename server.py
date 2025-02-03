bfaa11165fe589425ef35ce3d44785ba8363d36d
anikaguptas <anikaguptaindia@gmail.com>
2025-02-03 23:16:15 +0530

    first

---
+++ server.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS 
+import joblib
+
+
+app = Flask(__name__)
+
+
+CORS(app)
+
+mnb_model = joblib.load('mnb_model.pkl')  
+cv = joblib.load('cv_model.pkl')
+
+
+ 
+# Define a route for checking spam
+@app.route('/check_spam', methods=['POST'])
+def check_spam():
+    try:
+        data = request.get_json()
+        email_text = data.get('email', '')
+        if not email_text:
+            return jsonify({'error': 'Email text is required.'}), 400
+        
+        email_transformed = cv.transform([email_text])
+        prediction = mnb_model.predict(email_transformed)
+        print("prediction is " + str(prediction))
+        return jsonify({'prediction': int(prediction[0])})
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
